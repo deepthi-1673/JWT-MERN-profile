a55649b9d936739b2a7bfdca8117fed43750e502
Karanam Deepthi <120946600+deepthi-1673@users.noreply.github.com>
2023-01-27 00:28:35 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET:"TXNQzh7dckQ/Kb7Whw2IaYI/cHd7y7VRezalHeW0Qs=",
+    EMAIL:"herta72@ethereal.email",
+    PASSWORD:"ew9nyn15jFD32UMqSQ",
+    ATLAS_URI:"mongodb+srv://karanamdeepthi:deepthi1673@monuments.wlrw89p.mongodb.net/?retryWrites=true&w=majority"
+}
